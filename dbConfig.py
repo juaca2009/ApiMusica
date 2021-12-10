1f3f082a359ede6cca40a40932fe5c94f691ae91
juaca2009 <juaca2009@gmail.com>
2021-12-10 11:53:47 -0500

    configuracion pymongo

--- dbConfig.py
+++ dbConfig.py
@@ -1,8 +1,6 @@
 from app import app
-from flask_mongoalchemy import MongoAlchemy
+from flask_pymongo import PyMongo
 
-app.config['MONGOALCHEMY_CONNECTION_STRING'] = "mongodb+srv://juaca2009:12345@canciones.6yaip.mongodb.net/Canciones?retryWrites=true&w=majority"
-app.config['MONGOALCHEMY_DATABASE'] = 'Canciones'
-app.config['MONGOALCHEMY_USER'] = 'juaca2009'
-app.config['MONGOALCHEMY_PASSWORD'] = '12345'
-db = MongoAlchemy(app)
+app.config['MONGO_URI'] = "mongodb+srv://juaca2009:12345@canciones.6yaip.mongodb.net/Canciones?retryWrites=true&w=majority"
+cliente = PyMongo(app)
+db = cliente.db
