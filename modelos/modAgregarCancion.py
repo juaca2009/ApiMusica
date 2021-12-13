ef045a8fd148288de14959b5d697791afc6b3aab
juaca2009 <juaca2009@gmail.com>
2021-12-13 16:30:41 -0500

    configuracion swagger

--- modelos/modAgregarCancion.py
+++ modelos/modAgregarCancion.py
@@ -2,10 +2,10 @@ from flask_restx import fields
 from apiConfig import api
 
 modAgregarCancion = api.model('Agregar Canciones', {
-    'nombre': fields.String,'flt': fields.Float,
-    'lds': fields.Float,'alds': fields.Float, 
-    'strpk': fields.Float,'nrg': fields.Float,
-    'danc': fields.Float,'bpm': fields.Float,
-    'ptch': fields.Float,'mfcc': fields.List(fields.Float),
-    'genero': fields.String
+    'nombre': fields.String(default="La cancion"),'flt': fields.Float(default=0.99999),
+    'lds': fields.Float(default=0.99999),'alds': fields.Float(default=0.99999), 
+    'strpk': fields.Float(default=0.99999),'nrg': fields.Float(default=0.99999),
+    'danc': fields.Float(default=0.99999),'bpm': fields.Float(default=0.99999),
+    'ptch': fields.Float(default=0.99999),'mfcc': fields.List(fields.Float(dafault=[0.9999, 0.9999, 0.9999, 0.9999])),
+    'genero': fields.String(default="Un genero")
 })
